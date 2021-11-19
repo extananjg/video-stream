679c24e561de6e4644bb18075a9772603a91126e
extananjg <81245139+extananjg@users.noreply.github.com>
2021-11-19 14:25:56 +0700

    Update ytsearch.py

--- program/ytsearch.py
+++ program/ytsearch.py
@@ -34,13 +34,13 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search **needs an argument !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **Searching...**")
+        m = await message.reply_text("🔎 **Mencari...**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"🏷 **Name:** __{results[i]['title']}__\n"
-            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
+            text += f"🏷 **Nama Judul:** __{results[i]['title']}__\n"
+            text += f"⏱ **Durasi:** `{results[i]['duration']}`\n"
             text += f"👀 **Views:** `{results[i]['views']}`\n"
             text += f"📣 **Channel:** {results[i]['channel']}\n"
             text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
